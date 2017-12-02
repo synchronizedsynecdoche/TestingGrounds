ad5a6da11eda07fb727b84b900a4ecbb8a90c0e1
SynchronizedSynecdoche <kelly237@purdue.edu>
2017-12-02 17:03:19 -0500

    no longer mod13

---
+++ rot13.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"bufio"
+	"os"
+)
+func main() {
+
+	s := bufio.NewReader(os.Stdin)
+
+	userIn, _ := s.ReadString('\n')
+
+	var out string
+
+	alph := [26]string{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}
+	alphBig := [26]string{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}
+
+	for i :=0 ; i < len(userIn) ; i++ {
+
+		for j := 0 ; j < len(alph) ; j++ {
+
+			if string(userIn[i]) == alph[j]{
+
+				out += alph[ int(math.Mod(float64(j + i+1), 26))]
+			}
+			if string(userIn[i]) == alphBig[j]{
+
+				out += alph[ int(math.Mod(float64(j + i+1), 26))]
+			}
+		}
+
+	}
+	fmt.Print(out)
+}
