5d9448ff25021563a38b6d0999cd029bd06fe728
SynchronizedSynecdoche <kelly237@purdue.edu>
2017-12-31 20:03:14 -0500

    init

---
+++ structRef.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+
+        double x;
+        double y;
+        double mag;
+        } vector;
+
+void findMagnitude(vector *v){
+
+        *v.mag = sqrt( (v.x * v.x) + (v.y * v.y));
+
+        }
+
+int main(int argc, char* argv[]){
+
+        vector v;
+
+        v.x = 1;
+        v.y = 1;
+        v.mag = -1;
+        findMagnitude(&v);
+
+        printf("x:%f, y:%f, mag:%f",v.x,v.y,v.mag);
+
+        return 0;
+
+        }
