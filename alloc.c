82ae84bcae29a5d8fd85842f62e6204019d7891d
SynchronizedSynecdoche <kelly237@purdue.edu>
2018-01-01 20:00:38 -0500

    init

---
+++ alloc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+typedef struct {
+
+	char *name;
+	int gpa;
+	int isEnrolled;
+	}student;
+
+void enroll(student *s, int stat){
+	
+	if(stat != 0){
+		(*s).isEnrolled = 1;
+		}
+
+	else{
+		(*s).isEnrolled = 0;
+		}
+	}
+
+int main(int argc, char* argv[])}{
+	
+	student *s = malloc(sizeof(student));
+
+	s.name = "Patrick";
+	s.gpa = -1;
+	enroll(&s, 0);
+	
+	printf("%s : %d : %d", s.name, s.gpa, s.isEnrolled);
+	
+	//not freeing to see what it looks like under valgrind
+	
+	return 0;
+
