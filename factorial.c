86f914e31a502c8f2d46887969d871a13522408f
SynchronizedSynecdoche <kelly237@purdue.edu>
2018-01-05 10:58:24 -0500

    init

---
+++ factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//do this recursivelt
+//skeleton code
+int fac(int x){
+	
+	int y = x
+
+	if(x - 1 == 0){
+		return x;
+		}
+	}
+
+int main(int argc, char* argv[]){
+
+	int x = 3;
+
+	printf("%d",factorial(x));
+
+	return 0;
+	}
+
