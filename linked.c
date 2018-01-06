67ab2294f1ed6d8013c04a85621fffe856e18dd1
SynchronizedSynecdoche <kelly237@purdue.edu>
2018-01-06 14:56:52 -0500

    init

--- linked.c
+++ linked.c
@@ -6,6 +6,15 @@ typedef struct node {
 	struct node* next;
 	} node_t;
 
+void iter(node_t* n){
+
+	while(n->next != NULL){
+	
+		printf("%d\n",n->value);
+		n = n->next;
+		}
+	}
+
 int main(int argc, char* argv[]){
 	node_t* head = NULL;
 	head = malloc(sizeof(node_t));
@@ -14,6 +23,13 @@ int main(int argc, char* argv[]){
 	head->next = malloc(sizeof(node_t));
 
 	head->next->value = 1;
-	head->next->next = malloc(
+	head->next->next = malloc(sizeof(node_t));
+	
+	head->next->next->value = 2;
+	head->next->next->next = NULL; //terminate
+	
+	iter(head);
+	
+	return 0;
+	}
 
-//finish this
